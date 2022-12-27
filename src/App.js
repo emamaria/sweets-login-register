a6f70c3299100388dd2e8449c60364779a0b4625
emamaria <emascript@gmail.com>
2022-12-27 13:07:52 +0100

    ok register form basic feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './main.scss'
 
 function App() {
 
- 
+   console.log(process.env.REACT_APP_API)
   return (
     <div className="App">
     <AppRouter/> 
--- src/pages/forms/RegisterForm.jsx
+++ src/pages/forms/RegisterForm.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 
 const RegisterForm = () => {
+
+  
+  const [form, setForm] = useState({name: "", email: "", password: ""});
+
+  const {name, email, password} = form;
+
+  const handleChange = (e) => {
+    
+   setForm({...form, [e.target.name]: e.target.value })
+   console.log(form)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(form)
+  }
+
   return (
     <div className='form_container'>
-    <form className="form" action=""  method="post">
+    <form className="form" onSubmit={handleSubmit}>
    <h2 className='form_title'>Register</h2>
-   <input className='form_input' type="text" placeholder="name" name="name" required/> 
-   <input className='form_input' type="text" placeholder="email" name="email" required/> 
-   <input className='form_input' type="text" placeholder="password" name="password" required/>
+   <input className='form_input' type="text" placeholder="name" name="name" value={name} onChange={handleChange} required/> 
+   <input className='form_input' type="email" placeholder="email" name="email" value={email}  onChange={handleChange} required/> 
+   <input className='form_input' type="password" placeholder="password" name="password" value={password}  onChange={handleChange} required/>
    <button className='form_button' type="submit"><p>Register</p></button>
    <NavLink className="form_navlink" to="/login"><p className="registerLink">Back to login</p></NavLink>
   </form>
