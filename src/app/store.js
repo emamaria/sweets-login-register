d08e537b422d2ca5e92817f384ece484c81e2bea
emamaria <emascript@gmail.com>
2022-12-27 18:11:07 +0100

    ok authUser in store config

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartTasksReducer from '../features/cart/cartSlice'
+import authUserReducer from '../features/auth/authSlice'
 
 
 export const store = configureStore({
   reducer: {
     cartTasks: cartTasksReducer,
+    authUser: authUserReducer
   },
 })
 
