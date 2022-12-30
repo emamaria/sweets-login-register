e8a2ac8fb1963709d498637ef8ac82c02669b8e2
emamaria <emascript@gmail.com>
2022-12-30 14:07:40 +0100

    ok user and logout button dropdown menu

--- src/components/user/LoggedUserLink.jsx
+++ src/components/user/LoggedUserLink.jsx
@@ -3,7 +3,14 @@ import { NavLink } from 'react-router-dom'
 
 const LoggedUserLink = ({name}) => {
   return (
-    <NavLink className="user_link logged_user" to={`/${name}`}><div className='logged_user_container'>{`${name[0].toUpperCase()}${name.substring(1)}`}</div></NavLink>
+    <NavLink className="user_link logged_user" to={`/${name}`}>
+    <div className='dropdown'>
+    <button className="dropbtn">{`${name[0].toUpperCase()}${name.substring(1)}`}</button>
+    <div className="dropdown-content">
+    <NavLink className="dropdown-link" to="/">Logout</NavLink>
+    </div>
+    </div>
+    </NavLink>
   )
 }
 
