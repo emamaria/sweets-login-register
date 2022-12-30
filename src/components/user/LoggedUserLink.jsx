4129370856b213236817763431ae878ca9284f64
emamaria <emascript@gmail.com>
2022-12-30 16:14:08 +0100

    ok logout functionality and navigate to login

--- src/components/user/LoggedUserLink.jsx
+++ src/components/user/LoggedUserLink.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Navigate, NavLink } from 'react-router-dom'
 
 const LoggedUserLink = ({name}) => {
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    localStorage.clear()
+    window.location.reload()
+    Navigate("/login") 
+  }
   return (
     <div className='dropdown-container'>
     <div className='dropdown'>
     <NavLink className="dropbtn" to={`/${name}`}>{`${name[0].toUpperCase()}${name.substring(1)}`} </NavLink>
     <div className="dropdown-content">
-    <NavLink className="dropdown-link" to="/">Logout</NavLink>
+    <NavLink onClick={handleClick} className="dropdown-link">Logout</NavLink>
     </div>
     </div>
     </div>
